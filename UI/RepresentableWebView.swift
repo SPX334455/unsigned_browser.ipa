d074cb73809c0cb9b41ae89c50bc8903e1def48f
SPX334455 <119264181+SPX334455@users.noreply.github.com>
2026-02-05 23:43:44 +0300

    Delete UI/RepresentableWebView.swift

--- UI/RepresentableWebView.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-import WebKit
-
-struct RepresentableWebView: UIViewRepresentable {
-    let webView: WKWebView
-
-    func makeUIView(context: Context) -> WKWebView { webView }
-    func updateUIView(_ uiView: WKWebView, context: Context) {}
-}
